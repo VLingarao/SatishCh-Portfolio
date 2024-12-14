26180a8ce835c5642cf59cc93904b45fa0dffe66
VLingarao <andugulapatilingarao@gmail.com>
2024-12-14 15:19:30 +0530

    added projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,86 +1,71 @@
 import React from "react";
 import "../styles/Project.css";
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 
-import itsmeimg from '../assets/amanwithlapto.png';
-
-import project1 from "../assets/motionposter.png"
-import project2 from "../assets/poster01.jpg"
-import project3 from "../assets/poster03.png"
-import project4 from "../assets/motionposter02.png"
-import project5 from "../assets/poster04.png"
-import project6 from "../assets/poster02.jpg"
-import project7 from "../assets/poster06.png"
-import project8 from "../assets/poster05.png"
+import pro01 from "../assets/projects/poster01.png";
+import pro02 from '../assets/projects/ccodussehra.jpeg';
+import pro03 from '../assets/projects/ccoguntur.jpg';
+import pro04 from '../assets/projects/ccokidsdayhyd.jpg';
+import pro05 from '../assets/projects/expertdasara.jpg';
+import pro06 from '../assets/projects/fashion.jpg';
+import pro07 from '../assets/projects/fusion.jpg';
+import pro08 from '../assets/projects/growth.jpg';
+import pro09 from '../assets/projects/invixadd.jpg';
+import pro10 from '../assets/projects/invixbottle.jpg';
+import pro11 from '../assets/projects/invixdesign.jpg';
+import pro12 from '../assets/projects/invixmensday.jpg';
+import pro13 from '../assets/projects/invixzero.jpg';
+import pro14 from '../assets/projects/kovela.jpg';
+import pro15 from '../assets/projects/kovelatwo.jpg';
+import pro16 from '../assets/projects/mangobottle.jpg';
+import pro17 from '../assets/projects/okinawa.jpg';
+import pro18 from '../assets/projects/redmirchibottle.jpg';
+import pro19 from '../assets/projects/thanksgiving.jpg';
+import pro20 from '../assets/projects/tomatobottile.jpg';
 
 function Project() {
-
   const projects = [
-    {
-      img: project1,
-      name: "Motion Pictures",
-    },
-    {
-      img: project2,
-      name: "Construction Poster",
-    },
-    {
-      img: project3,
-      name: "Digital Marketing",
-    },
-    {
-      img: project4,
-      name: "Motion Pictures",
-    },
-    {
-      img: project8,
-      name: "Posters",
-    },
-    {
-      img: project5,
-      name: "Digital Marketing",
-    },
-    {
-      img: project6,
-      name: "Construction Poster",
-    },
-    {
-      img: project7,
-      name: "Posters",
-    },
+    { img: pro01},
+    { img: pro02},
+    { img: pro03},
+    { img: pro04},
+    { img: pro05},
+    { img: pro06},
+    { img: pro07},
+    { img: pro08},
+    { img: pro09},
+    { img: pro10},
+    { img: pro11},
+    { img: pro12},
+    { img: pro13},
+    { img: pro14},
+    { img: pro15},
+    { img: pro16},
+    { img: pro17},
+    { img: pro18},
+    { img: pro19},
+    { img: pro20},
   ];
 
   return (
     <div className="project-div-main" id="project-div-main" name="projects">
-      <div className="project-scend-div-container">
-        <div className="project-card-text-container">
-          <h1>
-            <span className="project-div-span">Proje</span>cts
-          </h1>
-          <div className="carousel-div">
-            <Carousel>
-              {projects.map((project, index)=>(
-                 <Carousel.Item key={index} interval={1000}>
-                 <div className="carousels-data-continer">
-                   <div className="carousel-img-div">
-                     <img src={project.img} alt={project.name} />
-                   </div>
-                   <h3>{project.name}</h3>
-                   <div className="buttons-continer">
-                     <a href={"https://drive.google.com/drive/folders/1f83zZ6pdH8KEsMs2Hw2EDXqv4s8irkN4"} target="_blank" style={{marginTop:"10px"}}>
-                       <button className="carousels-button-02">Live Demo</button>
-                     </a>
-                   </div>
-                 </div>
-               </Carousel.Item>
-              ))}
-              </Carousel>
-          </div>
-
-        </div>
-
-        <div className="project-pics-div">
-          <img src={itsmeimg} alt="itsmeimg" />
+      <div className="project-card-text-container">
+        <h1>
+          <span className="project-div-span">Proje</span>cts
+        </h1>
+        <div className="carousel-div">
+          <Carousel>
+            {projects.map((project, index) => (
+              <Carousel.Item key={index} interval={1000}>
+                <div className="carousels-data-container">
+                  <div className="carousel-img-div">
+                    <img src={project.img} alt={project.name} />
+                  </div>
+                  {/* <h3>{project.name}</h3> */}
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
         </div>
       </div>
     </div>
